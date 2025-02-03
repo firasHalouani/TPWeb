fe024f2ea2c4415166c8f4757f7e8396881549c6
firasHalouani <91092850+firasHalouani@users.noreply.github.com>
2025-02-03 16:59:33 +0100

    tp0 commit

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -7,11 +7,26 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 final class BlogController extends AbstractController{
-    #[Route('/blog', name: 'app_blog')]
+    #[Route('/', name: 'app_blog')]
     public function index(): Response
     {
         return $this->render('blog/index.html.twig', [
             'controller_name' => 'BlogController',
+        ]);
+    }
+
+    #[Route('/blog', name: 'blog_home')]
+    public function home($x = 10) 
+    {
+        return $this->render('blog/home.html.twig', [
+            'limit' => $x,
+        ]);
+    }
+    #[Route('/blog/{id}', name: 'blog_show')]
+    public function show($id)
+    {
+        return $this->render('blog/show.html.twig', [
+            'article_id' => $id,
         ]);
     }
 }
