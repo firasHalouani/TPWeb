dceae0f347065097efdfb9031c0ae09b337f3c65
firasHalouani <91092850+firasHalouani@users.noreply.github.com>
2025-02-17 17:09:26 +0100

    add show

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -2,31 +2,38 @@
 
 namespace App\Controller;
 
+use App\Entity\Article;
+use App\Repository\ArticleRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
-final class BlogController extends AbstractController{
+final class BlogController extends AbstractController
+{
     #[Route('/', name: 'app_blog')]
-    public function index(): Response
+    public function index(ArticleRepository $repo): Response
     {
+        $articles = $repo->findAll('Titre de l\'article');
+
         return $this->render('blog/index.html.twig', [
-            'controller_name' => 'BlogController',
+            'controller_name' =>
+                'BlogController',
+            'articles' => $articles,
         ]);
     }
 
     #[Route('/blog', name: 'blog_home')]
-    public function home($x = 10) 
+    public function home($x = 10)
     {
         return $this->render('blog/home.html.twig', [
             'limit' => $x,
         ]);
     }
     #[Route('/blog/{id}', name: 'blog_show')]
-    public function show($id)
+    public function show($id, ArticleRepository $repo)
     {
-        return $this->render('blog/show.html.twig', [
-            'article_id' => $id,
-        ]);
+        $article = $repo->find($id);
+
+        return $this->render('blog/show.html.twig', ['article' => $article]);
     }
 }
